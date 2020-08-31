3d929831ea2536b13078a7ad966c71b3ab1e835b
saul <josue.huamanT@gmail.com>
2020-08-31 16:55:02 -0500

    modificado1

---
+++ LongitudPalabra/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongitudPalabra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese Nombre y Apellidos: ");
+            string nombre = Console.ReadLine();
+
+            longitud(nombre);
+            Console.ReadKey();
+
+        }
+        static void longitud(string nombre)
+        {
+            Console.WriteLine("Longitud->  " + nombre.Length);
+        }
+    }
+}
